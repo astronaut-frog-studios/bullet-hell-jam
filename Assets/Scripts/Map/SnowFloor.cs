5ab0f96e07f3ca89188db5f0419135387a8938cc
MurilinhoPs <murilinhorps@gmail.com>
2023-05-02 14:41:01 -0300

    Create tilemap for 3d grid

--- Assets/Scripts/Map/SnowFloor.cs
+++ Assets/Scripts/Map/SnowFloor.cs
@@ -1,52 +1,54 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class SnowFloor : MonoBehaviour
+namespace Map
 {
-    public bool recover = false;
-    [SerializeField] private MeshRenderer meshRenderer;
-    [SerializeField] private Material groundMaterial;
-    [SerializeField] private Material snowMaterial;
-    
-
-
-    // Start is called before the first frame update
-    void Start()
+    public class SnowFloor : MonoBehaviour
     {
+        public bool recover;
+        [SerializeField] private MeshRenderer meshRenderer;
+        [SerializeField] private Material groundMaterial;
+        [SerializeField] private Material snowMaterial;
         
-    }
+        // Start is called before the first frame update
+        private void Start()
+        {
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
+        // Update is called once per frame
+        private void Update()
+        {
+        }
+
+        // voids normais
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                meshRenderer.material = groundMaterial;
+            }
+        }
         
-    }
-
-    // voids normais
-
-    void OnTriggerEnter ( Collider other ){
-        if(other.CompareTag("Player")){
-            meshRenderer.material =groundMaterial;
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player") && !recover)
+            {
+                StartCoroutine(BackToSnow());
+            }
         }
-    }
-    
 
-    void OnTriggerExit(Collider other) {
-        if(other.CompareTag("Player") && !recover){
-            StartCoroutine(BackToSnow());
+        private IEnumerator BackToSnow()
+        {
+            recover = true;
+            yield return new WaitForSeconds(2);
+            recover = false;
+            meshRenderer.material = snowMaterial;
         }
-    }
 
-
-    IEnumerator BackToSnow(){
-        recover = true;
-        yield return new WaitForSeconds(2);
-        recover = false;
-        meshRenderer.material = snowMaterial;
-    }
-
-    public bool HasSnow(){
-        return !recover;
+        public bool HasSnow()
+        {
+            return !recover;
+        }
     }
 }
