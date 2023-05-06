fa86255fce856501d054d384c53e32d0d60b4b3f
majulha <majulialcarvalho@gmail.com>
2023-05-06 19:08:55 -0300

    mould

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -5,11 +5,48 @@ using UnityEngine.Events;
 
 public class PlayerManager : MonoBehaviour
 {
-    public float Health { get; private set; }
+    //classe health e jogar a lógica lá
+    public float CurrentHealth { get; private set; }
     public float DamageToLight { get; private set; }
 
     [SerializeField] private string myTest;
     [SerializeField] private UnityEvent LessLight;
-    
+
+    private void Start()
+    {
+        /*
+        PlayerEvents.DamageReceived += ReceivedDamage;
+        PlayerEvents.PlayerDifficulty += PlayerDifficultyChange;
+
+        playerCollision = GetComponent<PlayerMeleeCollision>();
+
+        Health = playerObject.health;
+        Damage = playerObject.damage;
+        */
+    }
+
+    private void ReceivedDamage(float amountToLose, bool isMelee)
+    {
+        /*
+        if (isMelee)
+        {
+            playerCollision.DamageReceived();
+        }
+
+        Health -= amountToLose;
+        health = Health;
+
+        print("Health: " + Health);
+
+        if (Health <= 0)
+        {
+            GameManager.Instance.OnGameEnded(false);
+        }
+        */
+    }
+    public void Life()
+    {
+        
+    }
  
 }
